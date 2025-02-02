aebdaab4b1fec8f9b95d8fa04eabb977a9f1ee3a
mckenzie balaka <zakabalaka@yahoo.com>
2025-02-01 19:38:19 -0500

    Initial commit

---
+++ server/src/routes/api/weatherRoutes.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+const router = Router();
+
+// import HistoryService from '../../service/historyService.js';
+// import WeatherService from '../../service/weatherService.js';
+
+// TODO: POST Request with city name to retrieve weather data
+router.post('/', (req, res) => {
+  // TODO: GET weather data from city name
+  // TODO: save city to search history
+});
+
+// TODO: GET search history
+router.get('/history', async (req, res) => {});
+
+// * BONUS TODO: DELETE city from search history
+router.delete('/history/:id', async (req, res) => {});
+
+export default router;
